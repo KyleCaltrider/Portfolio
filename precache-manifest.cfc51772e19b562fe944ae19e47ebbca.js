3717e81d0461d835fbfbc053c2889cdeef5aab53
Kyle Caltrider <kyle@Kyles-MacBook-Pro.local>
2019-08-06 22:14:36 -0600

    Updates

---
+++ precache-manifest.cfc51772e19b562fe944ae19e47ebbca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "523fc46fd4a1ad025b57655a45d05f64",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e832cadd6d9cf46a80e5",
+    "url": "/static/css/main.2c7f4043.chunk.css"
+  },
+  {
+    "revision": "c7b6698205b0b1b6e74b",
+    "url": "/static/js/2.9d75758a.chunk.js"
+  },
+  {
+    "revision": "e832cadd6d9cf46a80e5",
+    "url": "/static/js/main.e9aa0334.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
